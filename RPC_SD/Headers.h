19d392091f961f284b1ea38c3d68b7031772fe61
Arthur <42arthursousa@gmail.com>
2018-11-12 15:47:21 -0200

    roando no VS

--- RPC_SD/Headers.h
+++ RPC_SD/Headers.h
@@ -8,8 +8,12 @@
 #include <list>
 #include <unordered_set>
 #include <unordered_map>
+#include<sstream>
 
 #include "color.h"
+
+
+#pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
 
 #define MAXNODES 4
 #define IP "127.0.0.1"
